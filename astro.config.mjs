3cbc446dc51f51f55c594e9628443ae317c84213
madjin <32600939+madjin@users.noreply.github.com>
2026-02-19 20:10:58 -0500

    chore: update tracked project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://elizaos.news',
   output: 'static',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     resolve: {
       alias: {
